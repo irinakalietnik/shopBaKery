cc9d51c30de96e1643bf57a2228083a1123dadd8
irinakalietnik <152523851+irinakalietnik@users.noreply.github.com>
2024-01-05 14:10:37 +0200

    button-close

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,12 @@
 const modal = document.querySelector(".backdrop");
-const modalBtnOpen = document.querySelector(".modal-btn-open");
-const modalBtnClose = document.querySelector(".modal__modal-btn-close");
+const modalBtnOpen = document.querySelectorAll(".modal-btn-open");
+
+const modalBtnClose = document.querySelector(".modal-btn-close");
 
 const toggleModal = () => modal.classList.toggle("backdrop--is-hidden");
 
-modalBtnOpen.addEventListener("click", toggleModal);
+modalBtnOpen.forEach((btn) => {
+  btn.addEventListener("click", toggleModal);
+});
+
 modalBtnClose.addEventListener("click", toggleModal);
